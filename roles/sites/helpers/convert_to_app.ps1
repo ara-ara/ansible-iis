4ca33def92a201e7b625783fe1b63de042554260
KENT\mrobbin7_stu <mrobbin7_stu@kent.edu>
2017-04-19 13:57:25 -0400

    Fixed helper script. Updated app descriptions

--- roles/sites/helpers/convert_to_app.ps1
+++ roles/sites/helpers/convert_to_app.ps1
@@ -3,12 +3,18 @@
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory=$True,Position=1)]
-        [string]$virtual_path,
+        [string]$site_name,
     [Parameter(Mandatory=$True,Position=2)]
+        [string]$virtual_path,
+    [Parameter(Mandatory=$True,Position=3)]
         [string]$app_pool
 )
 
 Import-Module WebAdministration
 
-ConvertTo-WebApplication -PSPath $virtual_path -ApplicationPool $app_pool
+$tmp = Get-WebApplication -Site $site_name -name $virtual_path
+$virtual_path = $virtual_path.Replace('/','\')
 
+if ($tmp -eq $null) { 
+    ConvertTo-WebApplication -PSPath IIS:\Sites\$site_name\$virtual_path -ApplicationPool $app_pool 
+}
